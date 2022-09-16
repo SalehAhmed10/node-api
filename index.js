1d0a6a9343461e2d083dd99bf24606d3680416e5
Saleh Ahmed <44835364+SalehAhmed10@users.noreply.github.com>
2022-09-16 18:47:01 +0500

    added projects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const importData = require('./data.json');
 
 const port = 5000;
 
@@ -9,6 +10,10 @@ app.use(express.urlencoded({ extended: false }));
 // Home route
 app.get("/", (req, res) => {
   res.send("Welcome to a basic express App");
+});
+
+app.get("/projects" , (req, res) => {
+    res.json(importData);
 });
 
 // Mock API
