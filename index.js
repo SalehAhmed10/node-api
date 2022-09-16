1dacdb837b0ea0a7074347a082326e54a3104251
Saleh Ahmed <44835364+SalehAhmed10@users.noreply.github.com>
2022-09-16 18:48:55 +0500

    front page

--- index.js
+++ index.js
@@ -7,22 +7,15 @@ const port = 5000;
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
-});
+// // Home route
+// app.get("/", (req, res) => {
+//   res.send("Welcome to a basic express App");
+// });
 
-app.get("/projects" , (req, res) => {
+app.get("/" , (req, res) => {
     res.json(importData);
 });
 
-// Mock API
-app.get("/users", (req, res) => {
-  res.json([
-    { name: "William", location: "Abu Dhabi" },
-    { name: "Chris", location: "Vegas" }
-  ]);
-});
 
 
 
